57883f4fa35b1e52dc050eeaee650f8ca880c23e
Muhammed Hanlaev <magamed220.ball@gmail.com>
2024-05-07 00:49:50 +0700

    new commit

--- 001.cpp
+++ 001.cpp
@@ -5,6 +5,7 @@ int main(int argc, const char *argv[])
 
     std::cout << "Hello World\n";
     std::cout << "Second commit\n";
+    std::cout << "new commit\n";
 
     system("pause");
     return 0;
